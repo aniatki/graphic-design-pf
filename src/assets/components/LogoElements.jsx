57fe4629ae46a6bdc5c818618056b96212cfd1a4
Hasan Atki <hasanatki@live.com>
2025-07-11 12:25:27 +0100

    Fix missing assets

--- src/assets/components/LogoElements.jsx
+++ src/assets/components/LogoElements.jsx
@@ -1,13 +1,17 @@
-import { logoElements } from '../projects/costcutter/logoElements.js'
+import triangle from "../images/costcutter/triangle.svg"
+import circle from "../images/costcutter/circle.svg"
 
 export default function LogoElements() {
     return (
         <section className="logo-elements">
-            {logoElements.map((elem, index) => (
-                <div key={index}>
-                    <img src={elem.imageSource} alt={elem.altText} />
-                    <p>{elem.description}</p>
-                </div>))}
+            <div>
+                <img src={triangle} alt="Triangle that is part of the logo" />
+                <p>mostly associated with stability</p>
+            </div>
+            <div>
+                <img src={circle} alt="Circle that is part of the logo" />
+                <p>associated with security, community and continuity</p>
+            </div>
         </section>
     )
 }
