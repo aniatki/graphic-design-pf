e7bcbcef0f3ef5ddb436d8471f61afde5a2d3143
Hasan Atki <hasanatki@live.com>
2025-07-17 22:50:54 +0100

    Remove void project links

--- src/assets/projects/projects.js
+++ src/assets/projects/projects.js
@@ -4,19 +4,19 @@ export const projects = [
         description: "Supermarket chain across Ireland.",
         to: "/costcutter",
     },
-    {
-        title: "Cosmic Brew",
-        description: "Coffee shop in outter space.",
-        to: "/",
-    },
-    {
-        title: "Kamela Nails",
-        description: "Nail salon in North Dublin.",
-        to: "/",
-    },
-    {
-        title: "Dreamwear",
-        description: "Online fashion shop.",
-        to: "/",
-    },
+    // {
+    //     title: "Cosmic Brew",
+    //     description: "Coffee shop in outter space.",
+    //     to: "/",
+    // },
+    // {
+    //     title: "Kamela Nails",
+    //     description: "Nail salon in North Dublin.",
+    //     to: "/",
+    // },
+    // {
+    //     title: "Dreamwear",
+    //     description: "Online fashion shop.",
+    //     to: "/",
+    // },
 ]
